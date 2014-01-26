747e72e447710dad4ecad3580d891e36fab1fe2d
José Pinto <zepinto@gmail.com>
2014-01-26 23:56:30 +0000

    Slightly improved StaticIMCConnection.

--- src/pt/lsts/imc/net/StaticIMCConnection.java
+++ src/pt/lsts/imc/net/StaticIMCConnection.java
@@ -38,171 +38,231 @@ import pt.lsts.imc.IMCMessage;
 import pt.lsts.neptus.messages.listener.MessageInfo;
 import pt.lsts.neptus.messages.listener.MessageListener;
 
+/**
+ * This class provides a simplified interface for sending and receiving IMC
+ * messages using a static (fixed ip/port) peer-to-peer connection through UDP.
+ * Example:<br/>
+ * <code><pre>
+ *  // Listen for IMC messages on port 6070 and all messages sent will go to 127.0.0.1:6002
+ *  StaticIMCConnection conn = new StaticIMCConnection("127.0.0.1", 6002, 6070);
+ *  
+ *  // activate listening for messages
+ *  conn.setPolling(true); 
+ *  
+ *  // wait up to 1 second to receive a message of type "EstimatedState"
+ *  IMCMessage m = conn.recv("EstimatedState", 1000); 
+ *  
+ *  if (m != null)
+ *      m.dump(System.out);     // Print out the message if it was received
+ *  
+ *  // Print all incoming Temperature messages to screen
+ *  while(true) {
+ *      System.out.println(conn.poll("Temperature"));
+ *  }
+ * </pre></code>
+ */
 public class StaticIMCConnection {
 
-    protected String remoteHost;
-    protected int localPort;
-    protected int remotePort;
-    protected boolean running = true;
-    
-    protected LinkedHashMap<Integer, IMCMessage> lastMessagesByType = new LinkedHashMap<Integer, IMCMessage>();
-    protected boolean polling = false;
-    protected MessageListener<MessageInfo, IMCMessage> pollingListener = new MessageListener<MessageInfo, IMCMessage>() {
-        @Override
-        public void onMessage(MessageInfo info, IMCMessage msg) {
-            lastMessagesByType.put(msg.getMgid(), msg);
-        }
-    };
-    
-    protected UDPTransport trans;
+	private String remoteHost;
+	private int remotePort;
 
-    public StaticIMCConnection(String remoteHost, int remotePort, int localPort) {
+	private LinkedHashMap<Integer, IMCMessage> lastMessagesByType = new LinkedHashMap<Integer, IMCMessage>();
+	private UDPTransport trans;
+	private boolean polling = false;
 
-        this.remoteHost = remoteHost;
-        this.remotePort = remotePort;
-        this.localPort = localPort;
+	private MessageListener<MessageInfo, IMCMessage> pollingListener = new MessageListener<MessageInfo, IMCMessage>() {
+		@Override
+		public void onMessage(MessageInfo info, IMCMessage msg) {
+			lastMessagesByType.put(msg.getMgid(), msg);
+		}
+	};
 
-        trans = new UDPTransport(localPort, 1);
-        trans.sendMessage(remoteHost, remotePort, new Heartbeat());
-    }
+	/**
+	 * Create a connection to a remote IMC host
+	 * 
+	 * @param remoteHost
+	 *            The IP address of the remote host. Examples:
+	 *            <code>"127.0.0.1"</code>, <code>"192.168.0.109"</code>.
+	 * @param remotePort
+	 *            The remote port to connect to.
+	 * @param localPort
+	 *            The local port to bind to and listen for incoming messages
+	 */
+	public StaticIMCConnection(String remoteHost, int remotePort, int localPort) {
 
-    public boolean send(IMCMessage m) {
-        m.setTimestamp(System.currentTimeMillis() / 1000.0);
-        return trans.sendMessage(remoteHost, remotePort, m);
-    }
+		this.remoteHost = remoteHost;
+		this.remotePort = remotePort;
 
-    public void stop() {
-        running = false;
-        trans.stop();
-    }
-    
-    
-    public void setPolling(boolean polling) {
-        boolean before = this.polling;
-        this.polling = polling;
-        lastMessagesByType.clear();
-        // if we started polling just now
-        if (polling && ! before) {
-            trans.addMessageListener(pollingListener);
-        }
-        else if (!polling) {
-           trans.removeMessageListener(pollingListener); 
-        }
-    }
+		trans = new UDPTransport(localPort, 1);
+		trans.sendMessage(remoteHost, remotePort, new Heartbeat());
+	}
 
-    protected IMCMessage message;
+	/**
+	 * Send an IMC message to the remote host
+	 * 
+	 * @param m
+	 *            The message to be sent
+	 * @return <code>true</code> if no error was found while sending. Since we
+	 *         are using UDP no guarantee is given in terms of successful
+	 *         message delivery, all we know is that the message was sent out.
+	 */
+	public boolean send(IMCMessage m) {
+		m.setTimestamp(System.currentTimeMillis() / 1000.0);
+		return trans.sendMessage(remoteHost, remotePort, m);
+	}
 
-    public synchronized IMCMessage recv(String abbrev, long timeoutMillis) {
-        message = null;
-        long targetMillis = System.currentTimeMillis() + timeoutMillis;
+	/**
+	 * Stop this connection and stops listening for messages.
+	 */
+	public void stop() {
+		trans.stop();
+	}
 
-        try {
-            MessageListener<MessageInfo, IMCMessage> listener = new MessageListener<MessageInfo, IMCMessage>() {
+	/**
+	 * Activates/deactivates polling mechanism for listening for incoming
+	 * messages. <br/>
+	 * When active, previous message of any type can be returned instantaneously
+	 * by using the method {@link #poll(String)}
+	 * 
+	 * @param polling
+	 *            Whether to use polling of messages.
+	 */
+	public void setPolling(boolean polling) {
+		boolean before = this.polling;
+		this.polling = polling;
+		lastMessagesByType.clear();
 
-                @Override
-                public synchronized void onMessage(MessageInfo info, IMCMessage msg) {
-                    try {
-                        message = msg;
-                        trans.removeMessageListener(this);
-                    }
-                    catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
+		if (polling && !before) {
+			trans.addMessageListener(pollingListener);
+		} else if (!polling) {
+			trans.removeMessageListener(pollingListener);
+		}
+	}
 
-            trans.addListener(listener, Arrays.asList(abbrev));
+	private IMCMessage message;
 
-            while (message == null && System.currentTimeMillis() < targetMillis) {
-                try {
-                    Thread.sleep(10);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            if (message != null) {
-                try {
-                    Class<?> c = Class.forName("pt.up.fe.dceg.neptus.imc." + abbrev);
-                    return (IMCMessage) c.getConstructor(IMCMessage.class).newInstance(message);
-                }
-                catch (Exception e) {
-                    return message;
-                }
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    
-    public IMCMessage poll(String abbrev) {
-        if (!polling)
-            setPolling(true);            
-        
-        int id = trans.getDefinition().getMessageId(abbrev);
-        if (id == -1)
-            return null;
-        
-        IMCMessage msg = lastMessagesByType.get(id);
-        if (msg == null)
-            return null;
-        try {
-            Class<?> c = Class.forName("pt.up.fe.dceg.neptus.imc." + abbrev);
-            return (IMCMessage) c.getConstructor(IMCMessage.class).newInstance(msg);
-        }
-        catch (Exception e) {
-            return msg;
-        }
-    }
+	/**
+	 * Synchronously wait for a message of a specific type
+	 * 
+	 * @param abbrev
+	 *            The message type you want to receive. Examples:
+	 *            <code>"Heartbeat"</code>, <code>EstimatedState</code>, etc.
+	 * @param timeoutMillis
+	 *            Maximum amount of time to wait for a message to be received in
+	 *            milliseconds after which <code>null</code> will be returned
+	 *            ([] in Matlab).
+	 * @return A message of given type or <code>null</code> if no new message of
+	 *         that type was received in <code>timeoutMillis</code> milliseconds
+	 */
+	public synchronized IMCMessage recv(String abbrev, long timeoutMillis) {
+		message = null;
+		long targetMillis = System.currentTimeMillis() + timeoutMillis;
 
-    public synchronized <T extends IMCMessage> T recv(Class<T> clazz, long timeoutMillis) {
-        message = null;
-        long targetMillis = System.currentTimeMillis() + timeoutMillis;
+		MessageListener<MessageInfo, IMCMessage> listener = new MessageListener<MessageInfo, IMCMessage>() {
 
-        try {
-            MessageListener<MessageInfo, IMCMessage> listener = new MessageListener<MessageInfo, IMCMessage>() {
+			@Override
+			public synchronized void onMessage(MessageInfo info,
+					IMCMessage msg) {
+				try {
+					message = msg;
+					trans.removeMessageListener(this);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
 
-                @Override
-                public synchronized void onMessage(MessageInfo info, IMCMessage msg) {
-                    try {
-                        message = msg;
-                        trans.removeMessageListener(this);
-                    }
-                    catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
+		trans.addListener(listener, Arrays.asList(abbrev));
 
-            trans.addListener(listener, Arrays.asList(clazz.getSimpleName()));
-            while (message == null && System.currentTimeMillis() < targetMillis) {
-                try {
-                    Thread.sleep(10);
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+		while (message == null && System.currentTimeMillis() < targetMillis) {
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return message;
+	}
 
-            if (message != null) {
-                return clazz.getConstructor(IMCMessage.class).newInstance(message);
-            }
-            return null;
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	/**
+	 * Retrieve the last received message of a given type. This method also
+	 * (re)activates polling if it is not currently active.
+	 * 
+	 * @param abbrev
+	 *            The type of message to be retrieved. Examples:
+	 *            <code>"Heartbeat"</code>, <code>EstimatedState</code>, etc.
+	 * @return The last received message of given type or <code>null</code> if
+	 *         no message of that type was yet received in this connection.
+	 * @see #setPolling(boolean)
+	 */
+	public IMCMessage poll(String abbrev) {
+		if (!polling)
+			setPolling(true);
 
-    public static void main(String[] args) throws Exception {
-        StaticIMCConnection seacon3 = new StaticIMCConnection("127.0.0.1", 6002, 6969);
-        while(true) {
-            System.out.println(seacon3.poll("EstimatedState"));
-            System.out.println(seacon3.poll("EntityState"));
-            Thread.sleep(100);
-        }      
-    }
+		int id = trans.getDefinition().getMessageId(abbrev);
+		if (id == -1)
+			return null;
+
+		IMCMessage msg = lastMessagesByType.get(id);
+		if (msg == null)
+			return null;
+		try {
+			return msg;
+		} catch (Exception e) {
+			return msg;
+		}
+	}
+
+	/**
+	 * Receive a message of given class. Similar to {@link #recv(String, long)}
+	 * but the message gets cast to selected class.
+	 * 
+	 * @param clazz
+	 *            The class of the message to be received.
+	 * @param timeoutMillis
+	 *            Amount of time to wait for a new message, in milliseconds.
+	 * @return A message of given class or <code>null</code> if no new message
+	 *         of that type was received in <code>timeoutMillis</code>
+	 *         milliseconds
+	 * @see #recv(String, long)
+	 */
+	public synchronized <T extends IMCMessage> T recv(Class<T> clazz,
+			long timeoutMillis) {
+		message = null;
+		long targetMillis = System.currentTimeMillis() + timeoutMillis;
+
+		try {
+			MessageListener<MessageInfo, IMCMessage> listener = new MessageListener<MessageInfo, IMCMessage>() {
+
+				@Override
+				public synchronized void onMessage(MessageInfo info,
+						IMCMessage msg) {
+					try {
+						message = msg;
+						trans.removeMessageListener(this);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			};
+
+			trans.addListener(listener, Arrays.asList(clazz.getSimpleName()));
+			while (message == null && System.currentTimeMillis() < targetMillis) {
+				try {
+					Thread.sleep(10);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (message != null) {
+				return clazz.getConstructor(IMCMessage.class).newInstance(
+						message);
+			}
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
