34b8d609d994f824d9a3f776bbce37014b79fde8
zepinto <zepinto@gmail.com>
2014-09-18 16:41:22 +0100

    Added build target to create ImcShell.jar (executable jar).

--- src-shell/pt/lsts/imc/cli/ImcShell.java
+++ src-shell/pt/lsts/imc/cli/ImcShell.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import javax.xml.bind.DatatypeConverter;
 
 import pt.lsts.imc.IMCDefinition;
 import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.ImcStringDefs;
+import pt.lsts.imc.lsf.LsfMessageLogger;
 import pt.lsts.imc.net.IMCProtocol;
 import pt.lsts.imc.net.UDPTransport;
+import pt.lsts.neptus.messages.listener.Consume;
+import pt.lsts.neptus.messages.listener.ImcConsumer;
 import asg.cliche.Command;
 import asg.cliche.Param;
 import asg.cliche.Shell;
@@ -22,18 +27,25 @@ public class ImcShell {
 	private LinkedHashMap<String, IMCMessage> vars = new LinkedHashMap<String, IMCMessage>();
 	private UDPTransport transport = new UDPTransport();
 	private IMCProtocol proto = null;
+	private Shell reference = null;
+	
+	@Consume
+	public void onMessage(IMCMessage m) {
+		vars.put(m.getSourceName() + "." + m.getAbbrev(), m);
+		LsfMessageLogger.log(m);
+	}
 
-	@Command(abbrev = "msgs", description = "Print the names of messages in the environment")
-	public void msgs() {
+	@Command(abbrev = "env", description = "Print the names of messages in the environment")
+	public void env() {
 		System.out.println(vars.keySet());
 	}
 
-	@Command(abbrev = "msg", description = "Print the value of a message in the environment")
-	public void msg(
-			@Param(name = "expr", description = "The name of the message to be printed or a value in the form <message>[.<field>]+") String msg) {
+	@Command(abbrev = "print", description = "Print the value of a message in the environment")
+	public void print(
+			@Param(name = "expr", description = "The name of the message to be printed or a value in the form <message>[/<field>]+") String msg) {
 
-		if (msg.contains(".")) {
-			String[] parts = msg.split("\\.");
+		if (msg.contains("/")) {
+			String[] parts = msg.split("\\/");
 			IMCMessage m = vars.get(parts[0]);
 			for (int i = 1; i < parts.length - 1; i++) {
 				if (m == null)
@@ -68,11 +80,46 @@ public class ImcShell {
 		vars.put(name, m);
 	}
 
+	private Object valueOf(String expr) {
+		if (vars.containsKey(expr))
+			return vars.get(expr);
+		else if (expr.contains("/")
+				&& vars.containsKey(expr.substring(0, expr.indexOf("/")))) {
+			String[] parts = expr.split("\\/");
+			IMCMessage m = vars.get(parts[0]);
+			for (int i = 1; i < parts.length - 1; i++) {
+				if (m == null)
+					break;
+				if (m.getTypeOf(parts[i]).equals("message"))
+					m = m.getMessage(parts[i]);
+			}
+			if (m != null)
+				return m.getValue(parts[parts.length - 1]);
+			else {
+				return null;
+			}
+		} else {
+			ScriptEngineManager mgr = new ScriptEngineManager();
+			ScriptEngine engine = mgr.getEngineByName("JavaScript");
+			try {
+				return engine.eval(expr);
+			} catch (Exception e) {
+				return null;
+			}
+		}
+	}
+
 	private void setValue(IMCMessage m, String field, String value)
 			throws Exception {
 		String fieldType = m.getTypeOf(field);
 		if (fieldType == null) {
 			throw new Exception("The field '" + field + "' is not valid.");
+		}
+
+		value = value.trim();
+		if (value.matches("`(.*)`")) {
+			m.setValue(field, valueOf(value.substring(1, value.length() - 1)));
+			return;
 		}
 
 		switch (fieldType) {
@@ -133,7 +180,7 @@ public class ImcShell {
 			if (!value.toLowerCase().equals("null")) {
 				String[] msgs = value.replace("[", "").replace("]", "")
 						.replace(" ", "").split(",");
-				ArrayList<IMCMessage> val = new ArrayList<>();
+				ArrayList<IMCMessage> val = new ArrayList<IMCMessage>();
 				for (String s : msgs) {
 					if (vars.containsKey(s))
 						val.add(vars.get(s).cloneMessage());
@@ -194,7 +241,7 @@ public class ImcShell {
 		vars.put(name, m);
 	}
 
-	@Command(name = "sleep", abbrev = "sleep", description = "Sleep for a number of seconds")
+	@Command(abbrev = "sleep", description = "Sleep for a number of seconds")
 	public void sleep(
 			@Param(name = "secs", description = "Time, in seconds, to sleep for") int secs) {
 		try {
@@ -202,6 +249,30 @@ public class ImcShell {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Command(abbrev = "send", description = "Send a message to a known IMC destination")
+	public void send(
+			@Param(name = "destination", description = "IMC name of the destination") String host,
+			@Param(name = "message", description = "Message to be sent (name of a message saved in this environment)") String message)
+			throws Exception {
+
+		if (proto == null)
+			throw new Exception(
+					"Discovery is not active. Use the command 'bind'.");
+
+		if (!Arrays.asList(proto.systems()).contains(host)) {
+			throw new Exception("The IMC destination '" + host
+					+ "' was not yet discovered.");
+		}
+		if (!vars.containsKey(message))
+			throw new Exception(
+					"There is no message in the environment named '" + message
+							+ "'.");
+
+		IMCMessage msg = vars.get(message);
+		proto.sendMessage(host, msg);
+		LsfMessageLogger.log(msg);
 	}
 
 	@Command(name = "send", description = "Send a message to a remote host", abbrev = "send")
@@ -214,35 +285,67 @@ public class ImcShell {
 			throw new Exception(
 					"There is no message in the environment named '" + message
 							+ "'.");
-		transport.sendMessage(host, port, vars.get(message));
-	}
-	
-	@Command
-	public void systems() {
-		if (proto == null)
-			System.out.println("[] (Use bind for listening to messages)");
-		else
-			System.out.println(Arrays.asList(proto.systems()));		
-	}
-	
-	@Command
-	public void get(String expr) {
 		
+		IMCMessage msg = vars.get(message);
+		transport.sendMessage(host, port, vars.get(message));
+		LsfMessageLogger.log(msg);		
 	}
-	
-	@Command
-	public void bind(int port) {
+
+	@Command(abbrev = "copy", description = "Create a copy of a message in the environment")
+	public void copy(
+			@Param(name = "src", description = "Name of the message to be copied") String src,
+			@Param(name = "dst", description = "Name of the message (copy) to be created") String dst) {
+		IMCMessage m = vars.get(src);
+		if (m != null)
+			m = m.cloneMessage();
+		vars.put(dst, m);
+	}
+
+	@Command(abbrev = "bind", description = "Discover another IMC peers and start receiving messages")
+	public void bind(
+			@Param(name = "port", description = "Local port where to listen") int port) {
 		if (proto != null)
 			proto.stop();
-		proto = new IMCProtocol(port);		
+		proto = new IMCProtocol(port);
+		proto.addMessageListener(ImcConsumer.create(this));
+	}
+
+	@Command(abbrev = "loop", description = "Execute a list of commands in a loop")
+	public void loop(
+			@Param(name = "count", description = "Number of times to execute (<= 0 for infinite loop)") int count,
+			@Param(name = "instructions", description = "List of instructions, separated by ;") String instr) {
+
+		String[] instrs = instr.split(";");
+		for (int i = 0; count <= 0 || i < count; i++) {
+			for (int j = 0; j < instrs.length; j++) {
+				try {
+					System.out.println(instrs[j]);
+					reference.processLine(instrs[j]);
+				}
+				catch (Exception e) {
+					System.err.println(e.getClass().getSimpleName()+": "+e.getMessage());
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @param reference the reference to set
+	 */
+	public void setReference(Shell reference) {
+		this.reference = reference;
 	}
 
 	public static void main(String[] args) throws IOException {
+		ImcShell imcShell = new ImcShell();
 		Shell shell = ShellFactory.createConsoleShell("?", "IMC Shell",
-				new ImcShell());
+				imcShell);
+		imcShell.setReference(shell);
+		
 		System.out.println("Using IMC v"
 				+ IMCDefinition.getInstance().getVersion() + " ("
-				+ ImcStringDefs.IMC_SHA + ").\nEnter ?help for usage information.");
+				+ ImcStringDefs.IMC_SHA
+				+ ").\nEnter ?help for usage information.");
 		shell.commandLoop();
 	}
 }
