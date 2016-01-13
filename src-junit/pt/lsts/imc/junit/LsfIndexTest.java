de821f45e0e25a5adbcf93f2066c7dcf4f6b5e60
Jose Pinto <zepinto@gmail.com>
2016-01-13 16:52:28 +0000

    Added another LsfIndex test.

--- src-junit/pt/lsts/imc/junit/LsfIndexTest.java
+++ src-junit/pt/lsts/imc/junit/LsfIndexTest.java
@@ -29,10 +29,14 @@
 package pt.lsts.imc.junit;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 
 import junit.framework.Assert;
+import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.lsf.LsfIndex;
 
 /**
@@ -41,10 +45,49 @@ import pt.lsts.imc.lsf.LsfIndex;
  */
 public class LsfIndexTest {
 
-	@Test
-	public void parseLsfTest() throws Exception {		
-		LsfIndex index = new LsfIndex(new File("samples/auv/Data.lsf"));
-		Assert.assertEquals(true, index.containsMessagesOfType("EstimatedState", "Announce"));
-		Assert.assertEquals(false, index.containsMessagesOfType("TrexOperation"));		
+	public List<File> samples() {
+		ArrayList<File> ret = new ArrayList<File>();
+		
+		File samples = new File("samples");
+		for (File log : samples.listFiles()) {
+			if (log.isDirectory() && new File(log, "Data.lsf").canRead()) {
+				ret.add(new File(log, "Data.lsf"));
+			}
+		}
+		return ret;
 	}
+	
+	@Test
+	public void parseLsfTest() throws Exception {
+		for (File log : samples()) {
+			long start = System.currentTimeMillis();
+			LsfIndex index = new LsfIndex(log);
+			Assert.assertEquals(true, index.containsMessagesOfType("EstimatedState", "Announce"));
+			Assert.assertEquals(false, index.containsMessagesOfType("TrexOperation"));
+			long end = System.currentTimeMillis();
+			System.out.println("Log with "+index.getNumberOfMessages()+" messages processed in "+(end-start)+" milliseconds.");
+		}
+	}
+	
+	@Test
+	public void getMessageAtTimeTest() throws Exception {		
+		LsfIndex index = new LsfIndex(new File("samples/auv/Data.lsf"));
+		double endTime = index.getEndTime() - 0.5;
+		double startTime = index.getStartTime() + 0.5;
+		Random r = new Random(System.currentTimeMillis());
+		for (int i = 0; i < 50; i++) {
+			double time = startTime + (endTime-startTime) * r.nextDouble();
+			IMCMessage msgAt = index.getMessageAt("EstimatedState", time);
+			Assert.assertNotNull(msgAt);
+			IMCMessage msgBefore = index.getMessageBeforeOrAt("EstimatedState", index.getNumberOfMessages()-1, time);
+			Assert.assertNotNull(msgBefore);
+			IMCMessage msgAfter = index.getMessageAtOrAfter("EstimatedState", 0, time);
+			Assert.assertNotNull(msgAfter);
+			Assert.assertTrue("Before <= At", msgBefore.getTimestamp() <= msgAt.getTimestamp());
+			Assert.assertTrue("At <= After", msgAt.getTimestamp() <= msgAfter.getTimestamp());
+			Assert.assertEquals(msgBefore.getTimestamp(), msgAfter.getTimestamp(), 0.5);
+		}		
+	}
+	
+	
 }
