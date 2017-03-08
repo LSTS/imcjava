9a3a1aba36fa01a1f432754ed3d6d28bf198fcfb
Jose Pinto <zepinto@gmail.com>
2017-03-08 10:07:14 +0000

    SequentialPlanExecution: Added option to send acoustic updates.

--- src-autonomy/pt/lsts/autonomy/SequentialPlanExecution.java
+++ src-autonomy/pt/lsts/autonomy/SequentialPlanExecution.java
@@ -28,13 +28,17 @@
  */
 package pt.lsts.autonomy;
 
+import java.io.IOException;
+
 import pt.lsts.imc.Abort;
+import pt.lsts.imc.AcousticOperation;
 import pt.lsts.imc.PlanControl;
 import pt.lsts.imc.PlanControl.OP;
 import pt.lsts.imc.PlanControl.TYPE;
 import pt.lsts.imc.PlanControlState;
 import pt.lsts.imc.PlanControlState.LAST_OUTCOME;
 import pt.lsts.imc.PlanControlState.STATE;
+import pt.lsts.imc.TextMessage;
 import pt.lsts.imc.VehicleState;
 import pt.lsts.imc.VehicleState.OP_MODE;
 import pt.lsts.imc.net.Consume;
@@ -54,25 +58,161 @@ public class SequentialPlanExecution {
 	TcpClient connection = null;
 	int index = 0;
 	
-	@Parameter
+	@Parameter(description="TCP hostname where to connect")
 	public String host = "127.0.0.1";
 	
-	@Parameter
+	@Parameter(description="TCP port where to connect")
 	public int port = 6002;
 	
-	@Parameter
+	@Parameter(description="Sequence of plan IDs to be executed")
 	public String[] plans = new String[] {"plan1", "plan2"};
 	
-	enum StateEnum {		
-		GettingReady,
-		StartingPlan,
-		Executing,
-		Finished,
-		Aborted;
+	@Parameter(description="Whether to send information messages via acoustic modem")
+	public boolean acousticUpdates = false;
+	
+	enum StateEnum {
+		GettingReady, 		// waiting for the vehicle to be in service mode
+		StartingPlan,		// waiting for the vehicle to be in maneuver mode
+		Executing,			// waiting for the plan execution to end
+		Finished,			// all plans have been executed successfully
+		Aborted;			// received an Abort
 	}
 	
-	StateEnum state = StateEnum.GettingReady;	
+	private StateEnum state = StateEnum.GettingReady;	
+	
+	{
+		System.out.println("STATE: "+state);
+	}
 
+	/**
+	 * Update stored message
+	 */
+	@Consume
+	void on(VehicleState msg) {
+		if (src == 0)
+			src = msg.getSrc();
+		
+		if (msg.getSrc() == src)
+			vehicleState = msg;			
+	}
+	
+	/**
+	 * Update stored message
+	 */
+	@Consume
+	void on(PlanControlState msg) {
+		if (msg.getSrc() == src)
+			planControlState = msg;
+	}
+	
+	/**
+	 * When an abort is received, stops everything
+	 */
+	@Consume
+	void on(Abort msg) {
+		state = StateEnum.Aborted;				
+	}
+	
+	/**
+	 * State machine step funtion
+	 */
+	@Periodic(1000)
+	public void step() {
+		
+		// connect if not already connected
+		if (connection == null) {
+			connect();
+			return;
+		}
+
+		// check if connection was lost
+		if (vehicleState.getAgeInSeconds() > 2 || planControlState.getAgeInSeconds() > 2) {
+			System.err.println("Reconnecting...");
+			connection.connect(host, port);
+			state = StateEnum.GettingReady;
+			System.out.println("STATE: "+state);
+			return;
+		}
+
+		// check if already done
+		if (index == plans.length) {
+			state = StateEnum.Finished;
+			try {
+				reportAcoustically("Finished all plans.");
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.exit(0);
+		}
+
+		// continue state machine execution
+		switch (state) {
+		case GettingReady:
+			if (vehicleState.getOpMode() == OP_MODE.SERVICE) {
+				System.out.println("Vehicle is ready for execution.");
+				try {
+					startExecution();
+					state = StateEnum.StartingPlan;
+					System.out.println("STATE: "+state);
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+					System.err.println(e.getClass().getSimpleName()+": "+e.getMessage());
+				}
+			}
+			break;
+		case StartingPlan:
+			if (planControlState.getState() == STATE.EXECUTING) {
+				System.out.println("Vehicle started executing "+plans[index]);
+				state = StateEnum.Executing;
+				System.out.println("STATE: "+state);
+				try {
+					reportAcoustically("Executing "+plans[index]);
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			else if (planControlState.getState() != STATE.INITIALIZING) {
+				System.out.println("Could not start "+plans[index]);
+				state = StateEnum.GettingReady;
+				System.out.println("STATE: "+state);
+			}
+			break;
+		case Executing:
+			if (planControlState.getState() != STATE.EXECUTING) {
+				if (planControlState.getLastOutcome() == LAST_OUTCOME.SUCCESS) {
+					System.out.println("Vehicle successfully executed "+plans[index]);
+					try {
+						reportAcoustically("Finished "+plans[index]);
+					}
+					catch (Exception e) {
+						e.printStackTrace();
+					}
+					index++;
+				}
+				else {
+					System.err.println("Error couldn't execute "+plans[index]);
+				}
+				state = StateEnum.GettingReady;
+				System.out.println("STATE: "+state);
+			}
+		default:
+			break;
+		}
+	}
+	
+	int reqId = 0;
+	void startExecution() throws Exception {
+		PlanControl pc = new PlanControl();
+		pc.setRequestId(++reqId);
+		pc.setType(TYPE.REQUEST);
+		pc.setOp(OP.START);
+		pc.setPlanId(plans[index]);
+		connection.send(pc);		
+	}
+	
 	private void connect() {
 		System.out.println("Connecting to "+host+":"+port);
 		try {
@@ -86,94 +226,15 @@ public class SequentialPlanExecution {
 		}
 	}
 	
-	@Consume
-	void on(VehicleState msg) {
-		if (src == 0)
-			src = msg.getSrc();
-		
-		if (msg.getSrc() == src)
-			vehicleState = msg;			
-	}
-	
-	@Consume
-	void on(PlanControlState msg) {
-		if (msg.getSrc() == src)
-			planControlState = msg;
-	}
-	
-	@Consume
-	void on(Abort msg) {
-		state = StateEnum.Aborted;			
-	}
-	
-	
-	int reqId = 0;
-	void startExecution() throws Exception {
-		PlanControl pc = new PlanControl();
-		pc.setRequestId(++reqId);
-		pc.setType(TYPE.REQUEST);
-		pc.setOp(OP.START);
-		pc.setPlanId(plans[index]);
-		connection.send(pc);		
-	}
-	
-	@Periodic(1000)
-	public void step() {
-		
-		if (connection == null) {
-			connect();
-			return;
-		}
-		
-		if (vehicleState.getAgeInSeconds() > 2 || planControlState.getAgeInSeconds() > 2) {
-			System.err.println("Reconnecting...");
-			connection.connect(host, port);
-			state = StateEnum.GettingReady;
-			return;
-		}
-				
-		if (index >= plans.length) {
-			state = StateEnum.Finished;
-		}
-			
-		System.out.println("STATE: "+state);
-		switch (state) {
-		case GettingReady:
-			if (vehicleState.getOpMode() == OP_MODE.SERVICE) {
-				System.out.println("Vehicle is ready for execution.");
-				try {
-					startExecution();
-					state = StateEnum.StartingPlan;
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-					System.err.println(e.getClass().getSimpleName()+": "+e.getMessage());
-				}
-			}
-			break;
-		case StartingPlan:
-			if (planControlState.getState() == STATE.EXECUTING) {
-				System.out.println("Vehicle started executing "+plans[index]);
-				state = StateEnum.Executing;
-			}
-			else if (planControlState.getState() != STATE.INITIALIZING) {
-				System.out.println("Could not start "+plans[index]);
-				state = StateEnum.GettingReady;
-			}
-			break;
-		case Executing:
-			if (planControlState.getState() != STATE.EXECUTING) {
-				if (planControlState.getLastOutcome() == LAST_OUTCOME.SUCCESS) {
-					System.out.println("Vehicle successfully executed "+plans[index]);
-					index++;
-				}
-				else {
-					System.err.println("Error couldn't execute "+plans[index]);
-				}
-				state = StateEnum.GettingReady;
-			}			
-		default:
-			break;
+	private void reportAcoustically(String text) throws IOException {
+		if (acousticUpdates) {
+			TextMessage msg = new TextMessage();
+			msg.setText(text);
+			AcousticOperation op = new AcousticOperation();
+			op.setOp(AcousticOperation.OP.MSG);
+			op.setSystem("broadcast");
+			op.setMsg(msg);
+			connection.send(op);
 		}
 	}
 	
